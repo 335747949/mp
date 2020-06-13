a326daa9599df73982c48dd89b796610f22d4b4e
lanwei <335747949@qq.com>
2020-06-13 17:54:35 +0800

    feat: debug调试程序

---
+++ src/main/java/com/wy/mp/entity/Debug.java
@@ -0,0 +1,18 @@
+package com.wy.mp.entity;
+
+/**
+ * @author lanwei
+ * @email 335747949@qq.com
+ */
+
+/**
+ * debug调试程序：
+ *
+ * f8:逐行执行程序
+ * f7:进入方法中
+ * shift+f8：跳出方法
+ * f9:进入下一个断点
+ * ctrl+f2：退出debug模式，停止程序
+ */
+public class Debug {
+}
